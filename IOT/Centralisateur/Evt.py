ff91ba4d366f7044dc013b244e07b301fd72e0bc
Valouzze <valentin.ganiez@gmail.com>
2021-06-10 21:40:27 +0000

    Ajout de l'evt

---
+++ IOT/Centralisateur/Evt.py
@@ -0,0 +1,24 @@
+import sys
+sys.path.insert(1, '/home/test/XMPP/')
+from XMPP_API import XMPP_Offline
+import threading
+import time
+
+client = XMPP_Offline("evt", "valouzze.local", "test")
+
+def sending():
+    global client
+    while True:
+        consumme = client.getLastMessage()
+        if consumme != None :
+            sender = consumme[1]
+            msg = consumme[0]
+            print("sender : " + str(sender) + " msg: " + str(msg))
+            client.send_message(msg, "web", "valouzze.local")
+            client.send_message(msg, "tampon", "valouzze.local")
+
+receive = threading.Thread(target=client.receive_message)
+receive.start()
+
+sending_message = threading.Thread(target=sending())
+sending.start()
